7a5a9e7c96913fa90a5eba10a9df30a69f609b16
Janne Mattila <janne.mattila@hotmail.fi>
2023-11-28 12:43:21 +0200

    Update demo

--- storage/main.tf
+++ storage/main.tf
@@ -11,21 +11,29 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.resource_group.name
   address_space       = ["10.0.0.0/16"]
 
-  subnet {
-    name           = "snet-1"
-    address_prefix = "10.0.0.0/24"
-  }
+  # subnet {
+  #   name           = "snet-1"
+  #   address_prefix = "10.0.0.0/24"
+  # }
 
-  subnet {
-    name           = "snet-2"
-    address_prefix = "10.0.1.0/24"
-  }
+  # subnet {
+  #   name           = "snet-2"
+  #   address_prefix = "10.0.1.0/24"
+  # }
 
   lifecycle {
     ignore_changes = [
       subnet
     ]
   }
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                                      = "snet-1"
+  resource_group_name                       = azurerm_resource_group.resource_group.name
+  virtual_network_name                      = azurerm_virtual_network.vnet.name
+  address_prefixes                          = ["10.0.1.0/24"]
+  private_endpoint_network_policies_enabled = false
 }
 
 resource "azurerm_storage_account" "storage" {
@@ -35,7 +43,9 @@ resource "azurerm_storage_account" "storage" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
-  public_network_access_enabled = false
+  public_network_access_enabled   = false
+  allow_nested_items_to_be_public = false
+
   network_rules {
     default_action             = "Deny"
     ip_rules                   = []
